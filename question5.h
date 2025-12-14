08ee2230c5917dc5ff3fac955de9883168467cf5
AlejoMG42 <ale82942@gmail.com>
2025-12-14 19:30:25 +0100

    Question 5

---
+++ question5.h
@@ -0,0 +1,38 @@
+#ifndef QUESTION_5_H
+#define QUESTION_5_H
+
+#include <unistd.h>
+
+#define MAX_BUFFER_SIZE 128
+#define EXIT_CMD "exit"
+
+// System Messages
+#define WELCOME_MSG "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n"
+#define PROMPT_MSG "enseash % "
+#define BYE_MSG "Bye bye...\n"
+#define ERR_FORK "Error: Fork failed\n"
+#define ERR_CMD "Command not found\n"
+#define ERR_READ "Error: Read failed\n"
+#define PREVIOUS_CMD "a_program"
+
+#define PROMPT_BUFFER_SIZE 128
+#define PROMPT_BASE "enseash % "
+#define PROMPT_EXIT_FMT "enseash [exit:%d|%ldms] %% "
+#define PROMPT_SIGN_FMT "enseash [sign:%d|%ldms] %% "
+
+#define MILISECONDS_PER_SECONDS  1000      
+#define NANOSECONDS_PER_MILISECONDS   1000000
+
+// Displays the initial welcome message
+void display_welcome_message();
+
+// Displays the shell prompt
+void display_prompt();
+
+// Exits the shell cleanly with a message
+void exit_shell();
+
+// Executes the command using fork/exec/wait pattern
+int execute_command(char *command, long *execution_time);
+
+#endif
